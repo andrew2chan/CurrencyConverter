9176d5ab851309c8d26334e677a025ee8ae37ff3
Andrew Chan <chan.andrew1995@gmail.com>
2021-02-12 18:04:42 -0500

    revert react scripts

---
+++ build/precache-manifest.c9edaa723b004579367a77b62a1dc065.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cf60aab67383d6a787aad307911c2526",
+    "url": "/CurrencyConverter/index.html"
+  },
+  {
+    "revision": "f0a2e0d48ab8693e7152",
+    "url": "/CurrencyConverter/static/css/main.76e27cc9.chunk.css"
+  },
+  {
+    "revision": "9aa7d682bd3a5808e6a3",
+    "url": "/CurrencyConverter/static/js/2.ca414bc8.chunk.js"
+  },
+  {
+    "revision": "6ff874e4b897db4bb989",
+    "url": "/CurrencyConverter/static/js/3.fe4abebe.chunk.js"
+  },
+  {
+    "revision": "f0a2e0d48ab8693e7152",
+    "url": "/CurrencyConverter/static/js/main.dba887f8.chunk.js"
+  },
+  {
+    "revision": "6cd764f8b24edfa7846c",
+    "url": "/CurrencyConverter/static/js/runtime~main.30f11593.js"
+  }
+]);
